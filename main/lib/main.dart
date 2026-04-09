10f1d1dfb41ab7034ea7050ffd18d74e9f1a76da
Wildtypers <wildtypers@gmail.com>
2026-04-08 23:54:44 -0700

    Adding custom spacing within Widgets

--- main/lib/main.dart
+++ main/lib/main.dart
@@ -59,6 +59,7 @@ class _HomePageState extends State<MyHomePage>{
               ],
             ),
             Row(
+              spacing: 15,
               children: [
                 Column(
                   children: [
