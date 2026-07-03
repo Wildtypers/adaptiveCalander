9fc0b1a227dfb3924fce57151c0f0fffbf45d714
Wildtypers <wildtypers@gmail.com>
2026-07-02 22:33:54 -0700

    Fixing Previous Issue with Routing

--- main/lib/calanderPage.dart
+++ main/lib/calanderPage.dart
@@ -9,6 +9,12 @@ class calendarPage extends StatefulWidget{
   const calendarPage({super.key});
 
   @override
+
+  State<calendarPage> createState() => _calendarPageState();
+}
+
+class _calendarPageState extends State<calendarPage>{
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Calander Page')),
